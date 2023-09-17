c1ceec1ad875f90f4ddd80527ff3c7644c121998
Federico Molina <federico.molina.146@gmail.com>
2023-09-17 19:29:06 -0300

    se logran modificaciones en el css y se agrega la funcionalidad de creacion de tablas desde el formulario

--- main.js
+++ main.js
@@ -34,29 +34,51 @@ function createTask(value) {
     })  */
   /*   hasta esta parte se logra el resultado deseado agregando los nuevos task en el array task  */
 }
-
-function renderTask(){
+function renderTask() {
+    // Borra los elementos existentes en el tBody
     tBody.innerHTML = '';
+
     tasks.forEach(task => {
+        console.log(task);
+        // Crea una nueva fila (tr) para la tarea
         const tr = document.createElement("tr");
         tr.id = task.id;
+        tr.classList.add("tabla");
+
+        // Crea los botones "Start" y "Status"
         const btnStart = document.createElement("button");
-        btnStart.innerHTML = 'start';
+        btnStart.innerHTML = 'Start';
         btnStart.id = "btnStart";
         const btnStatus = document.createElement("button");
+        btnStatus.innerHTML = "incomplete";
+        btnStart.classList.add("table-buton")
+        btnStatus.classList.add("table-button")
+
+        // Crea las celdas (td) para la fila
         const tdStart = document.createElement("td");
         const tdTitle = document.createElement("td");
-        const tdState = document.createElement("td");
         const tdModify = document.createElement("td");
+        tdStart.classList.add("table-buton");
+        tdModify.classList.add("table-buton");
+        tdTitle.classList.add("table-title");
+        tdTitle.innerHTML = task.title;
+        
+        
+
+        // Agrega los botones a las celdas correspondientes
         tdStart.appendChild(btnStart);
         tdModify.appendChild(btnStatus);
-        
+
+        // Agrega las celdas a la fila
         tr.appendChild(tdStart);
-        
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdModify);
+
+        // Agrega la fila completa al tBody
+        tBody.appendChild(tr);
+
         console.log(tr);
-        
-        tBody.appendChild(tr)
-        console.log(tBody)
-    })
+        console.log(tBody);
+    });
 }
 
