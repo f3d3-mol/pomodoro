91211435edd1002238530c34e7241fd12eb1881f
Federico Molina <federico.molina.146@gmail.com>
2023-09-17 15:09:55 -0300

    se agregan la funcionalidad del 'create task' en el js

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ let idTask = 0;
 const bAdd = document.querySelector("#bAdd");
 const itTask = document.querySelector("#itTask");
 const form = document.querySelector("#form");
+const tBody = document.querySelector("#tasks");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (itTask.value != "") {
     createTask(itTask.value);
     itTask.value = "";
-    /* renderTask(); */
+    renderTask();
   }
 });
 
@@ -34,13 +35,28 @@ function createTask(value) {
   /*   hasta esta parte se logra el resultado deseado agregando los nuevos task en el array task  */
 }
 
-
-/* function renderTask(){
-    const tasksContainer = document.querySelector('#tasks');
-
-    const html =tasks.map(task => {        
-    });
-    
-    tasksContainer = html.join('');
+function renderTask(){
+    tBody.innerHTML = '';
+    tasks.forEach(task => {
+        const tr = document.createElement("tr");
+        tr.id = task.id;
+        const btnStart = document.createElement("button");
+        btnStart.innerHTML = 'start';
+        btnStart.id = "btnStart";
+        const btnStatus = document.createElement("button");
+        const tdStart = document.createElement("td");
+        const tdTitle = document.createElement("td");
+        const tdState = document.createElement("td");
+        const tdModify = document.createElement("td");
+        tdStart.appendChild(btnStart);
+        tdModify.appendChild(btnStatus);
+        
+        tr.appendChild(tdStart);
+        
+        console.log(tr);
+        
+        tBody.appendChild(tr)
+        console.log(tBody)
+    })
 }
- */
+
